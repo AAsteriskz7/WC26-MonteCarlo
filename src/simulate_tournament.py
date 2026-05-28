f2889f4b633feded46de9c862e65ea703636d95d
AAsteriskz7 <avsadarsh7@gmail.com>
2026-05-28 14:15:08 -0700

    tournament simulation file

---
+++ src/simulate_tournament.py
@@ -0,0 +1,11 @@
+# Goal: Simulate the 2026 World Cup 10,000 times using Monte Carlo simulation and trained Poisson models.
+
+# Step 1: Setup & Ingestion
+
+# Step 2: Core Match Simulation (simulate_match)
+
+# Step 3: Group Stage Logic (simulate_group_stage)
+
+# Step 4: Knockout Stage Logic (simulate_knockout_stage)
+
+# Step 5: The Monte Carlo Loop (main execution)
